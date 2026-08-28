a5b19910cfd7e9529920788583fff05f7ef9f4a6
aatle <168398276+aatle@users.noreply.github.com>
2026-08-28 11:25:43 -0700

    Implement balance bot in main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from math import sin
-from time import monotonic, sleep
+from math import radians
+from time import monotonic
 
-import adafruit_mpu6050
 import board
 
+from filtered_mpu6050 import FilteredMPU6050
 from h_bridge_motor import HBridgeMotor
+from pid_controller import PIDController
 
-# i2c = board.I2C()
-# mpu = adafruit_mpu6050.MPU6050(i2c)
+i2c = board.I2C()
+
+filter_weight = 0.95
+mpu = FilteredMPU6050(
+    i2c,
+    filter_weight=filter_weight,
+    axes=(2, 0, 1),
+    signs=(-1, 1, -1),
+)
 
 motor = HBridgeMotor(
     in1=12,
@@ -15,21 +23,35 @@ motor = HBridgeMotor(
     pwm=16,
 )
 
-while True:
-    # acceleration = mpu.acceleration
-    # gyro = mpu.gyro
-    # temperature = mpu.temperature
-    # print(f"Accel X: {acceleration[0]:.2f}")
-    # print(f"Accel Y: {acceleration[1]:.2f}")
-    # print(f"Accel Z: {acceleration[2]:.2f}")
-    # print(f"Gyro X:  {gyro[0]:.2f}")
-    # print(f"Gyro Y:  {gyro[1]:.2f}")
-    # print(f"Gyro Z:  {gyro[2]:.2f}")
-    # print(f"Temp C:  {temperature:.2f}")
-    # print()
+kp = 0.0
+ki = 0.0
+kd = 0.0
 
-    motor.move(sin(monotonic()))
-    # motor.move(0.5)
-    # motor.move(1.0)
-    # sleep(1)
-    # print(monotonic())
+pid = PIDController(kp, ki, kd)
+
+
+tick_rate = 60.0
+
+abort_angle = radians(45.0)
+
+
+def main() -> None:
+    last_time = monotonic()
+    while True:
+        current_time = monotonic()
+        dt = current_time - last_time
+        if dt < 1.0 / tick_rate:
+            continue
+        last_time = current_time
+        mpu.update(dt)
+        pitch = mpu.pitch
+        if abs(pitch) > abort_angle:
+            print("Robot fell. Aborting.")
+            motor.stop()
+            break
+        speed = pid.calculate(dt, pitch)
+        motor.move(speed)
+
+
+if __name__ == "__main__":
+    main()
