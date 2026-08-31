9c122ea4166a55e41cd601f7d9f521a7e9484d13
aatle <168398276+aatle@users.noreply.github.com>
2026-08-31 14:23:54 -0700

    Add zero divsion protection in EMA

--- independent_ema.py
+++ independent_ema.py
@@ -11,6 +11,6 @@ class IndependentEMA:
     def update(self, dt: float, measurement: float) -> float:
         if dt <= 0.0:
             return self.value
-        alpha = 1.0 - math.exp(-dt / self.tau)
+        alpha = 1.0 - math.exp(-dt / self.tau) if self.tau != 0.0 else 1.0
         self.value += alpha * (measurement - self.value)
         return self.value
