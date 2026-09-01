0dad0d4b1bcd115ee3b0b20a29e93202b9fb26c5
aatle <168398276+aatle@users.noreply.github.com>
2026-09-01 15:45:21 -0700

    Tune constants more

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 kp = 0.60
-ki = 0.00
-kd = 0.0035
+ki = 0.30
+kd = 0.0025
 ks = 0.00
 pitch_tau = 0.0025
 speed_tau = 0.0
-velocity_correction = -1.0
+velocity_correction = -0.0
 
-setpoint = -5.40
+setpoint = -5.50
 
 tick_rate = 250.0
 
