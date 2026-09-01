6aa8c3c2bc253c40c5fd13aa90fd7e3fd4809a82
aatle <168398276+aatle@users.noreply.github.com>
2026-09-01 14:18:00 -0700

    Good constants version

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
-kp = 0.80
+kp = 0.50
 ki = 0.30
 kd = 0.0015
 ks = 0.00
-pitch_tau = 0.005
-speed_tau = 0.005
-velocity_correction = 0.1
+pitch_tau = 0.0025
+speed_tau = 0.0
+velocity_correction = 0.05
 
-setpoint = -5.60
+setpoint = -5.50
 
 tick_rate = 250.0
 
 abort_angle = 45.0
 
-complementary_filter_weight = 0.95
+complementary_filter_weight = 0.98
