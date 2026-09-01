c992e73cb772c16fbbb9b1793086244ffc7a9d2f
aatle <168398276+aatle@users.noreply.github.com>
2026-09-01 15:32:52 -0700

    Constants testing version

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
-kp = 0.50
-ki = 0.30
-kd = 0.0015
+kp = 0.60
+ki = 0.00
+kd = 0.0035
 ks = 0.00
 pitch_tau = 0.0025
 speed_tau = 0.0
-velocity_correction = 0.05
+velocity_correction = -1.0
 
-setpoint = -5.50
+setpoint = -5.40
 
 tick_rate = 250.0
 
