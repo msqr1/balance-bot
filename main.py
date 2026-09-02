418cea130228c5fed7a018ffa6555eab00f57080
aatle <168398276+aatle@users.noreply.github.com>
2026-09-02 16:02:28 -0700

    Change signs for new mpu placement

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ mpu = FilteredMPU6050(
     digital_filter,
     filter_bandwidth=Bandwidth.BAND_21_HZ,
     axes=(2, 0, 1),
-    signs=(-1, 1, -1),
+    signs=(1, 1, 1),
     calibrated_centers=(0.4767, -0.0322, 0.1800),
     calibrated_scales=(1.0012, 0.9939, 0.9802),
     calibrated_gyro_offsets=(-0.050926, 0.021809, 0.007153),
