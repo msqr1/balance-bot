1a28b750c4ee04ae5f367ee21c46a948fab1db31
aatle <168398276+aatle@users.noreply.github.com>
2026-09-02 21:59:38 -0700

    Tune constants version alpha

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ kp = 0.60
 ki = 0.00
 kd = 0.006
 ks = 0.0
-kv = 0.2
+kv = 0.8
 pid_tau = 0.00
 pitch_rate_tau = 0.010
 speed_tau = 0.00
-kv_tau = 0.020
+kv_tau = 0.50
 velocity_correction = 0.00
 
 setpoint = -4.80
