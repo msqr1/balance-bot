5e20376fd85920f4d32ea9750e1a084eecccf649
aatle <168398276+aatle@users.noreply.github.com>
2026-09-02 21:20:11 -0700

    Reset constants

--- constants.py
+++ constants.py
@@ -1,22 +1,22 @@
-kp = 0.50
-ki = 0.04
-kd = 0.005
-ks = 0.05
+kp = 0.60
+ki = 0.00
+kd = 0.00
+ks = 0.0
 kv = 0.0
 pid_tau = 0.00
-pitch_rate_tau = 0.015
+pitch_rate_tau = 0.010
 speed_tau = 0.00
 velocity_correction = 0.00
 
-setpoint = -4.00
+setpoint = -3.00
 
-tick_rate = 100.0
+tick_rate = 200.0
 
 abort_angle = 45.0
 
-kalman_q_angle = 0.0001
+kalman_q_angle = 0.00001
 kalman_q_bias = 0.003
-kalman_r_measure = 0.03
+kalman_r_measure = 5.0
 
 # complementary_filter_weight = 0.98
 
