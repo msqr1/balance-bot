bd2561dafa99f635850dba811a481c624e1a271d
aatle <168398276+aatle@users.noreply.github.com>
2026-09-03 10:22:14 -0700

    Tune constants version 0.3

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
-kp = 0.60
+kp = 0.50
 ki = 0.00
 kd = 0.006
-ks = 0.0
+ks = 0.10
 kv = 0.8
 pid_tau = 0.00
 pitch_rate_tau = 0.010
 speed_tau = 0.00
-velocity_tau = 0.50
+kv_tau = 0.50
 velocity_correction = 0.10
 
 setpoint = -5.20
@@ -15,13 +15,13 @@ tick_rate = 200.0
 
 abort_angle = 45.0
 
-kalman_q_angle = 0.00001
+kalman_q_angle = 0.0001
 kalman_q_bias = 0.003
 kalman_r_measure = 5.0
 
 # complementary_filter_weight = 0.98
 
-left_multiplier = 1.0
+left_multiplier = 0.87
 right_multiplier = 1.0
 
 enable_motors = True
