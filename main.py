153f91817a9a8c7639cd747877413230ab1924fa
aatle <168398276+aatle@users.noreply.github.com>
2026-09-03 16:30:36 -0700

    Add raw and filtered distance printing

--- main.py
+++ main.py
@@ -158,7 +158,8 @@ def main() -> None:
             # print(mpu)
             print(
                 f"Setpoint: {pid.setpoint:.3f}, Distance error: {distance_error:.2f} cm"
-                f", Raw distance: {distance:.2f} cm"
+                f", Raw distance: {-1.0 if raw_distance is None else raw_distance} cm"
+                f", Filtered distance: {distance:.2f} cm"
             )
             last_print = current_time
         for group, loggers in grouped_loggers.items():
